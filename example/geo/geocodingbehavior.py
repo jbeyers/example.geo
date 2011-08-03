f6b06b1b4368f440998c9963bac4b3058a085adf
Johan Beyers <jbeyers@juizi.com>
2011-08-03 17:01:40 +0200

    Actual geocoding added

--- example/geo/geocodingbehavior.py
+++ example/geo/geocodingbehavior.py
@@ -1,13 +1,18 @@
+import urllib
+import simplejson
+import transaction
+
+from five import grok
+
+
 from zope.interface import alsoProvides, implements
 from zope.component import adapts
 from zope import schema
+from zope.lifecycleevent import IObjectCreatedEvent, IObjectModifiedEvent
 from plone.directives import form
 from plone.dexterity.interfaces import IDexterityContent
 from plone.autoform.interfaces import IFormFieldProvider
 
-from plone.namedfile import field as namedfile
-from z3c.relationfield.schema import RelationChoice, RelationList
-from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from example.geo import MessageFactory as _
 
@@ -18,6 +23,24 @@ class IGeocodingBehavior(form.Schema):
     """
    
     # -*- Your Zope schema definitions here ... -*-
+
+    address = schema.TextLine(
+            title=_('Address'),
+            description=_('Full address, separated by commas.'),
+            )
+
+    lat = schema.Float(
+            title=_('Latitude'),
+            description=_('Latitude in decimal format. Clear to automatically generate.'),
+            required=False,
+            )
+
+    lng = schema.Float(
+            title=_('Longitude'),
+            description=_('Longitude in decimal format. Clear to automatically generate.'),
+            required=False,
+            )
+
 
 
 alsoProvides(IGeocodingBehavior,IFormFieldProvider)
@@ -42,3 +65,25 @@ class GeocodingBehavior(object):
         self.context = context
 
     # -*- Your behavior property setters & getters here ... -*-
+
+def geocode(obj, event):
+    if not obj.lat or not obj.lng:
+        address = ', '.join([getattr(obj, i) for i in
+                  ['street', 'suburb', 'town', 'postal_code'] if getattr(obj, i)])
+        url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&region=au&sensor=false' % urllib.quote(address)
+        geocode = urllib.urlopen(url).read()
+        data = simplejson.loads(geocode)
+        if data['status'] == 'OK':
+            if data['results']:
+                location = data['results'][0]['geometry']['location']
+                obj.lat = float(location['lat'])
+                obj.lng = float(location['lng'])
+                transaction.commit
+
+@grok.subscribe(IGeocodingBehavior, IObjectCreatedEvent)
+def supplier_created(obj, event):
+    geocode(obj, event)
+
+@grok.subscribe(IGeocodingBehavior, IObjectModifiedEvent)
+def supplier_modified(obj, event):
+    geocode(obj, event)
